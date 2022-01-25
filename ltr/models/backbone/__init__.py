9b6f17d21cb955625fc2d131d7db8472c136756b
xuedong he <849459832@qq.com>
2022-01-25 14:39:17 +0800

    update

--- ltr/models/backbone/__init__.py
+++ ltr/models/backbone/__init__.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-from .resnet import resnet18, resnet50
-=======
-from .resnet import resnet18, resnet34, resnet50, resnet_baby
->>>>>>> 67ba55b4c540787997f7b894164fe2911ecc2778
+from .resnet import resnet18, resnet34, resnet50
+
 from .resnet18_vggm import resnet18_vggmconv1

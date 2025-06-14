979595356bfce1f1a8ddd8d8155646876a6800e8
hexdjx <hexuedong@zjnu.edu.cn>
2025-06-14 11:04:33 +0800

    update prompt

--- ltr/models/target_classifier/linear_filter.py
+++ ltr/models/target_classifier/linear_filter.py
@@ -405,3 +405,127 @@ class ProbLinearFilter(nn.Module):
 
         return weights, weights_iter, losses
 
+# Prompt Tracking--------------------------------------------------------------
+class ProLinearFilter(nn.Module):
+    """Target classification filter module.
+    args:
+        filter_size:  Size of filter (int).
+        filter_initialize:  Filter initializer module.
+        filter_optimizer:  Filter optimizer module.
+        feature_extractor:  Feature extractor module applied to the input backbone features."""
+
+    def __init__(self, filter_size, filter_initializer, filter_optimizer=None, feature_dim=256, out_dim=512,
+                 norm_scale=1.0, mlp_dim=3):
+        super().__init__()
+
+        self.filter_size = filter_size
+
+        # Modules
+        self.filter_initializer = filter_initializer
+        self.filter_optimizer = filter_optimizer
+
+        ########################################################
+        # my add classification and prompt attention fusion
+        # clf_feature_extractor = conv_bn_relu(4*feature_dim, out_dim)
+
+        # pro_feature_extractor = clf_features.encoder_mlp(in_dim=mlp_dim, hid_dim=16)  # 1*22*22
+
+        # pro_feature_extractor = conv_bn_relu(mlp_dim, out_dim, kernel_size=16, stride=16, padding=0)
+
+        # fuse_conv = nn.Sequential(nn.Conv2d(out_dim*2 , out_dim, kernel_size=1, padding=0, bias=False),  # cat: out_dim*2
+        #                           InstanceL2Norm(scale=norm_scale))
+
+        # self.feature_extractor = nn.Sequential(clf_feature_extractor, pro_feature_extractor, fuse_conv)
+
+        self.feature_extractor = clf_features.PE_ATT(norm_scale=norm_scale)  # PE_ATT PE_MLP
+        ########################################################
+
+        # Init weights
+        for m in self.feature_extractor.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def forward(self, train_feat, test_feat, train_prob, test_prob, train_bb, *args, **kwargs):
+        """Learns a target classification filter based on the train samples and return the resulting classification
+        scores on the test samples.
+        The forward function is ONLY used for training. Call the individual functions during tracking.
+        args:
+            train_feat:  Backbone features for the train samples (4 or 5 dims).
+            test_feat:  Backbone features for the test samples (4 or 5 dims).
+            trian_bb:  Target boxes (x,y,w,h) for the train samples in image coordinates. Dims (images, sequences, 4).
+            *args, **kwargs:  These are passed to the optimizer module.
+        returns:
+            test_scores:  Classification scores on the test samples."""
+
+        assert train_bb.dim() == 3
+
+        num_sequences = train_bb.shape[1]
+
+        # Extract features
+        train_feat = self.extract_classification_feat(train_feat, train_prob, num_sequences)
+        test_feat = self.extract_classification_feat(test_feat, test_prob, num_sequences)
+
+        # Train filter
+        filter, filter_iter, losses = self.get_filter(train_feat, train_bb, *args, **kwargs)
+
+        # Classify samples using all return filters
+        test_scores = [self.classify(f, test_feat) for f in filter_iter]
+
+        return test_scores
+
+    def extract_classification_feat(self, feat, pro, num_sequences=None):
+        """Extract classification features based on the input backbone features."""
+        if self.feature_extractor is None:
+            return feat
+
+        if pro.dim() == 5:
+            pro = pro.reshape(-1, *pro.shape[-3:])
+
+        # output = self.feature_extractor[2](self.feature_extractor[0](feat) * self.feature_extractor[1](pro))
+
+        # output = self.feature_extractor[2](self.feature_extractor[0](feat) + self.feature_extractor[1](pro))  # add
+
+        # output = self.feature_extractor[2](torch.cat([self.feature_extractor[0](feat), self.feature_extractor[1](pro)], dim=1)) # cat
+
+        output = self.feature_extractor(feat, pro)
+
+        if num_sequences is None:
+            return output
+
+        return output.reshape(-1, num_sequences, *output.shape[-3:])
+
+    def classify(self, weights, feat):
+        """Run classifier (filter) on the features (feat)."""
+
+        scores = filter_layer.apply_filter(feat, weights)
+
+        return scores
+
+    def get_filter(self, feat, bb, *args, **kwargs):
+        """Outputs the learned filter based on the input features (feat) and target boxes (bb) by running the
+        filter initializer and optimizer. Note that [] denotes an optional dimension.
+        args:
+            feat:  Input feature maps. Dims (images_in_sequence, [sequences], feat_dim, H, W).
+            bb:  Target bounding boxes (x, y, w, h) in the image coords. Dims (images_in_sequence, [sequences], 4).
+            *args, **kwargs:  These are passed to the optimizer module.
+        returns:
+            weights:  The final oprimized weights. Dims (sequences, feat_dim, wH, wW).
+            weight_iterates:  The weights computed in each iteration (including initial input and final output).
+            losses:  Train losses."""
+
+        weights = self.filter_initializer(feat, bb)
+
+        if self.filter_optimizer is not None:
+            weights, weights_iter, losses = self.filter_optimizer(weights, feat=feat, bb=bb, *args, **kwargs)
+        else:
+            weights_iter = [weights]
+            losses = None
+
+        return weights, weights_iter, losses
+
